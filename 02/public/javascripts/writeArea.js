59a75f44a5df927b9e6a47fd8a49a8cfb7c37587
mmhyalsoft <m.mongelli@hylasoftusa.com>
2017-02-28 14:05:12 -0600

    02 text area added

---
+++ 02/public/javascripts/writeArea.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Marco on 2/28/2017.
+ */
+$( document ).ready(function() {
+
+    $('.elementContainer').click(function() {
+      $('.elementContainer').fadeOut(100);
+      $('#content').show(600);
+    });
+
+});
