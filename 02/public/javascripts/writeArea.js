0af66986d055df75ba1271cd219a6237bc2037e9
mmhyalsoft <m.mongelli@hylasoftusa.com>
2017-02-28 15:05:59 -0600

    email and calendar added

--- 02/public/javascripts/writeArea.js
+++ 02/public/javascripts/writeArea.js
@@ -12,11 +12,17 @@ $( document ).ready(function() {
        //alert($('input[name="myRadio"]:checked', '#myForm').val());
        if($('input[name="when"]:checked', '#myForm').val()==="later") {
          console.log("later selected");
+         $('#datepicker').hide(200);
+         $('#email').show(200);
        }
 
        if($('input[name="when"]:checked', '#myForm').val()==="date") {
          console.log("date selected");
+         $('#datepicker').show(200);
+         $('#email').hide(200);
        }
     });
 
+    //date picker
+    var picker = new Pikaday({ field: document.getElementById('datepicker') });
 });
